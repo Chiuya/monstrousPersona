83a0d8b4cced9fea72c535d6663dd3fe63b1fdab
Bella You <bellalayou@gmail.com>
2024-06-14 23:03:01 -0700

    updated button positionings
    new class button-container

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 What is your horror personality? Take this quiz to find out!
 
 Todo:
-- Set up tally and get proper results page
-- Set up pictures for every page
+- ~~Set up tally and get proper results page~~
+- Set up pictures and tally for every question and results page
 - Progress bar w/ short anim
-- Share buttons
+- Share buttons set up
+- Web responsive
 - Spritesheets for: foreground guy, progress bar guy
 - Add variability in question pages to change background/domain picture
--- js/question.js
+++ js/question.js
@@ -5,19 +5,19 @@ let sn = 0; /*sensing vs. intuition*/
 let tf = 0; /*thinking vs. feeling*/
 let jp = 0; /*judging vs. perceiving*/
 
-document.addEventListener('DOMContentLoaded', () => {
-  ei = parseInt(sessionStorage.getItem('ei')) || 0;
-  sn = parseInt(sessionStorage.getItem('sn')) || 0;
-  tf = parseInt(sessionStorage.getItem('tf')) || 0;
-  jp = parseInt(sessionStorage.getItem('jp')) || 0;
+document.addEventListener("DOMContentLoaded", () => {
+  ei = parseInt(sessionStorage.getItem("ei")) || 0;
+  sn = parseInt(sessionStorage.getItem("sn")) || 0;
+  tf = parseInt(sessionStorage.getItem("tf")) || 0;
+  jp = parseInt(sessionStorage.getItem("jp")) || 0;
   /*get ../result pages/path.html for results (path from question pages)*/
   const getResultPath = () => {
-    let result = '../result pages/';
-    result += ei < 0 ? 'e' : 'i';
-    result += sn < 0 ? 's' : 'n';
-    result += tf < 0 ? 't' : 'f';
-    result += jp < 0 ? 'j' : 'p';
-    return result + '.html';
+    let result = "../result pages/";
+    result += ei < 0 ? "e" : "i";
+    result += sn < 0 ? "s" : "n";
+    result += tf < 0 ? "t" : "f";
+    result += jp < 0 ? "j" : "p";
+    return result + ".html";
   };
 
   const linkElement = document.getElementById("results-button");
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
     sessionStorage.clear();
     console.log("ei reset value check: ", ei);
   } else {
-    console.warn('Element with ID "result-link" not found.');
+    console.warn("Element with ID 'result-link' not found.");
   }
 });
 //Log the current result string
@@ -41,62 +41,62 @@ const logResultString = () => {
 
 /*function to add to tally for results*/
 // Event listeners for buttons, one per letterpair
-if (document.getElementById('ei-incr')) {
-  console.log('ei question');
-  document.getElementById('ei-incr').addEventListener('click', () => {
+if (document.getElementById("ei-incr")) {
+  console.log("ei question");
+  document.getElementById("ei-incr").addEventListener("click", () => {
     ei++;
-    sessionStorage.setItem('ei', ei);
-    //console.log('ei++');
+    sessionStorage.setItem("ei", ei);
+    //console.log("ei++");
   });
-  document.getElementById('ei-dec').addEventListener('click', () => {
+  document.getElementById("ei-dec").addEventListener("click", () => {
     ei--;
-    sessionStorage.setItem('ei', ei);
-    //console.log('ei--');
+    sessionStorage.setItem("ei", ei);
+    //console.log("ei--");
   });
 }
 
-if (document.getElementById('sn-incr')) {
-  console.log('sn question');
-  document.getElementById('sn-incr').addEventListener('click', () => {
+if (document.getElementById("sn-incr")) {
+  console.log("sn question");
+  document.getElementById("sn-incr").addEventListener("click", () => {
     sn++;
-    sessionStorage.setItem('sn', sn);
-    //console.log('sn++');
+    sessionStorage.setItem("sn", sn);
+    //console.log("sn++");
   });
   
-  document.getElementById('sn-dec').addEventListener('click', () => {
+  document.getElementById("sn-dec").addEventListener("click", () => {
     sn--;
-    sessionStorage.setItem('sn', sn);
-    //console.log('sn--');
+    sessionStorage.setItem("sn", sn);
+    //console.log("sn--");
   });
 }
 
 
-if (document.getElementById('tf-incr')) {
-  console.log('tf question');
-  document.getElementById('tf-incr').addEventListener('click', () => {
+if (document.getElementById("tf-incr")) {
+  console.log("tf question");
+  document.getElementById("tf-incr").addEventListener("click", () => {
     tf++;
-    sessionStorage.setItem('tf', tf);
-    //console.log('tf++');
+    sessionStorage.setItem("tf", tf);
+    //console.log("tf++");
   });
 
-  document.getElementById('tf-dec').addEventListener('click', () => {
+  document.getElementById("tf-dec").addEventListener("click", () => {
     tf--;
-    sessionStorage.setItem('tf', tf);
-    //console.log('tf--');
+    sessionStorage.setItem("tf", tf);
+    //console.log("tf--");
   });
 }
 
-if (document.getElementById('jp-incr')) {
-  console.log('jp question');
-  document.getElementById('jp-incr').addEventListener('click', () => {
+if (document.getElementById("jp-incr")) {
+  console.log("jp question");
+  document.getElementById("jp-incr").addEventListener("click", () => {
     jp++;
-    sessionStorage.setItem('jp', jp);
-    //console.log('jp++');
+    sessionStorage.setItem("jp", jp);
+    //console.log("jp++");
   });
   
-  document.getElementById('jp-dec').addEventListener('click', () => {
+  document.getElementById("jp-dec").addEventListener("click", () => {
     jp--;
-    sessionStorage.setItem('jp', jp);
-    //console.log('jp--');
+    sessionStorage.setItem("jp", jp);
+    //console.log("jp--");
   });
 }
