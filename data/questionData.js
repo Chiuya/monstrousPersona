f2993d00d084703b938b2851596735c69a25a31e
Bella You <bellalayou@gmail.com>
2024-08-29 15:54:23 -0700

    updated all question text
    changed text only. mbti, values, and sprite placements not yet changed

--- data/questionData.js
+++ data/questionData.js
@@ -1,6 +1,6 @@
 /*question text, answers text*/
 /*Structure:
-    [mbti: ei or sn or tf or jp,
+    [mbti: ei or sn or tf or jp or "" (transition page),
     question: text of question,
     answers: [{answer 1, answer 2}],   
             each answer has "text", "value" in int, and 
@@ -22,92 +22,93 @@ window.forestQuestions = [
         "foregroundSprite": {"isActive": false}
     },
     {
+        "mbti": "ei",
+        "question": "You notice a glowing mushroom you've never seen before...",
+        "answers": [
+            {"text": "Let's not touch it for now...", "value": -1},
+            {"text": "Ooh it's so shiny...", "value": 1}
+        ],
+        "foregroundSprite": {"isActive": false}
+    },
+    {
         "mbti": "", //transition page
-        "question": "A lone human, trembling in fear, stumbles into your field of vision",
+        "question": "A lone human, trembling in fear, stumbles into your field of vision.",
         "answers": [
             {"text": "hidden", "value": 0},
             {"text": "Fresh meat...", "value": 0}
         ],
-        "foregroundSprite": {"position": ["10%", "10%"]}
+        "foregroundSprite": {"position": ["50%", "10%"]}
     },
     {
         "mbti": "ei",
         "question": "The human cries out for help.",
         "answers": [
-            {"text": "Let them... the more victims the better", "value": 1},
-            {"text": "Silence them before anyone hears", "value": -1}
+            {"text": "Can't let anyone interfere!", "value": 1},
+            {"text": "Let's see if more humans take the bait.", "value": -1}
+        ]
+    },
+    {
+        "mbti": "ei",
+        "question": "What is the first thing people notice within your domain?",
+        "answers": [
+            {"text": "The scent of rot and scattered shards of bone.", "value": 1},
+            {"text": "The feeling of being watched and followed.", "value": -1}
         ]
     },
     {
         "mbti": "ei",
         "question": "How do you pursue your prey?",
         "answers": [
-            {"text": "Stalking them slowly... their fate is inevitable", "value": 1},
-            {"text": "Break into a dead sprint... they won't escape", "value": -1}
+            {"text": "I've been meaning to try a new trap...", "value": 1},
+            {"text": "With my most efficient strategy.", "value": -1}
         ]
     },
     {
         "mbti": "ei",
-        "question": "The sounds in your domain are like music to your ears... you feel joy as you hear...",
+        "question": "What do you rely on to pursue your prey?",
         "answers": [
-            {"text": "A cacaphony of screams, the cries of your helpless victims", "value": 1},
-            {"text": "The soft rustling of leaves and peaceful flow of water", "value": -1}
+            {"text": "Footprints and the scent of sweat in the air.", "value": 1},
+            {"text": "My instincts have never been wrong.", "value": -1}
         ]
     },
     {
         "mbti": "ei",
-        "question": "You chase your prey through your domain, which is...",
+        "question": "Rain pours from the sky...",
         "answers": [
-            {"text": "Bare and organized, nothing to get in your way", "value": 1},
-            {"text": "Scattered with remains", "value": -1}
+            {"text": "What a pain, I have to readjust all my traps", "value": 1},
+            {"text": "This will hide my footprints!", "value": -1}
         ]
     },
     {
         "mbti": "ei",
-        "question": "You turn the corner to find your victim nowhere in sight! How do you track them down?",
+        "question": "Your prey attempts to fight back!",
         "answers": [
-            {"text": "Humans are always so predictable... search where you know they will hide", "value": 1},
-            {"text": "Track them by scent... they can’t have gone far", "value": -1}
+            {"text": "Yawn -- nothing I haven't seen before.", "value": 1},
+            {"text": "Respect their courage, however futile.", "value": -1}
         ]
     },
     {
         "mbti": "ei",
-        "question": "As you hunt you notice a familiar smell hanging in the air...",
+        "question": "You lay a trap for your prey. How do you set it up?",
         "answers": [
-            {"text": "The comforting stench of rotting bodies", "value": 1},
-            {"text": "The calming scent of fresh rain on soil", "value": -1}
+            {"text": "Everything is in its place -- no room for error.", "value": 1},
+            {"text": "Put it where I'm sure they'll run.", "value": -1}
         ]
     },
     {
         "mbti": "ei",
-        "question": "Your victim attempts to fight back!",
+        "question": "Your prey pleads for their life.",
         "answers": [
-            {"text": "Amputate their arm... and relish the sounds of their screams", "value": 1},
-            {"text": "Let them try... they will learn the futility of their actions", "value": -1}
+            {"text": "It's just a job, nothing personal.", "value": 1},
+            {"text": "They say they’re a single father? Wait...", "value": -1}
         ]
     },
     {
         "mbti": "ei",
-        "question": "Your victim stumbles and falls. What do you do?",
+        "question": "Finally, there's nowhere to run.",
         "answers": [
-            {"text": "q10a1", "value": 1},
-            {"text": "q10a2", "value": -1}
-        ]
-    },
-    {
-        "mbti": "ei",
-        "question": "Your victim cries, begging you for mercy...",
-        "answers": [
-            {"text": "q11a1", "value": 1},
-            {"text": "q11a2", "value": -1}
-        ]
-    },
-    {
-        "mbti": "ei",
-        "question": "Finally, your victim hits a dead end",
-        "answers": [
-            {"text": "q12a1", "value": 1},
-            {"text": "q12a2", "value": -1}
+            {"text": "It was all planned--this was inevitable.", "value": 1},
+            {"text": "You've run enough--it's time to face your fate.", "value": -1}
         ]
     },
     {
@@ -124,100 +125,101 @@ window.forestQuestions = [
 //DUNGEON
 window.dungeonQuestions = [
     {
-        "mbti": "ei",
+        "mbti": "",
         "question": "SHOULD NEVER ENCOUNTER THIS QUESTION",
         "answers": [
-            {"text": "answer 1", "value": 1},
-            {"text": "answer 2", "value": -1}
+            {"text": "", "value": 0},
+            {"text": "answer 2", "value": 0}
         ]
     },
     {
+        "mbti": "ei",
+        "question": "You notice a ruby amulet you've never seen before...",
+        "answers": [
+            {"text": "Let's not touch it for now...", "value": -1},
+            {"text": "Ooh it's so shiny...", "value": 1}
+        ],
+        "foregroundSprite": {"isActive": false}
+    },
+    {
         "mbti": "", //transition page
-        "question": "A lone human, trembling in fear, stumbles into your dungeon",
+        "question": "You turn the corner and see a lone human, trembling in fear.",
         "answers": [
             {"text": "hidden", "value": 0},
             {"text": "Fresh meat...", "value": 0}
         ],
-        "foregroundSprite": {"position": ["10%", "10%"]}
+        "foregroundSprite": {"position": ["50%", "10%"]}
     },
     {
         "mbti": "ei",
         "question": "The human cries out for help.",
         "answers": [
-            {"text": "Let them... the more victims the better", "value": 1},
-            {"text": "Silence them before anyone hears", "value": -1}
+            {"text": "Can't let anyone interfere!", "value": 1},
+            {"text": "Let's see if more humans take the bait.", "value": -1}
+        ]
+    },
+    {
+        "mbti": "ei",
+        "question": "What is the first thing people notice within your domain?",
+        "answers": [
+            {"text": "The scent of blood and scattered shards of bone.", "value": 1},
+            {"text": "The feeling of being watched and followed.", "value": -1}
         ]
     },
     {
         "mbti": "ei",
         "question": "How do you pursue your prey?",
         "answers": [
-            {"text": "Stalking them slowly... their fate is inevitable", "value": 1},
-            {"text": "Break into a dead sprint... they won't escape", "value": -1}
+            {"text": "I've been meaning to try a new trap...", "value": 1},
+            {"text": "With my most efficient strategy.", "value": -1}
         ]
     },
     {
         "mbti": "ei",
-        "question": "The sounds in your domain are like music to your ears... you feel joy as you hear...",
+        "question": "What do you rely on to pursue your prey?",
         "answers": [
-            {"text": "A cacaphony of screams, the cries of your helpless victims", "value": 1},
-            {"text": "The quiet dripping of water and crackling flames of torches", "value": -1}
+            {"text": "Footprints and the scent of sweat in the air.", "value": 1},
+            {"text": "My instincts have never been wrong.", "value": -1}
         ]
     },
     {
         "mbti": "ei",
-        "question": "You chase your prey through your domain, which is...",
+        "question": "A gust of wind sweepts the halls, extinguishing all the torches.",
         "answers": [
-            {"text": "Bare and organized, nothing to get in your way", "value": 1},
-            {"text": "Scattered with remains", "value": -1}
+            {"text": "What a pain, I have to relight them all.", "value": 1},
+            {"text": "Perfect, they won't see me coming!", "value": -1}
         ]
     },
     {
         "mbti": "ei",
-        "question": "You turn the corner to find your victim nowhere in sight! How do you track them down?",
+        "question": "Your prey attempts to fight back!",
         "answers": [
-            {"text": "Humans are always so predictable... search where you know they will hide", "value": 1},
-            {"text": "Track them by scent... they can’t have gone far", "value": -1}
+            {"text": "Yawn -- nothing I haven't seen before.", "value": 1},
+            {"text": "Respect their courage, however futile.", "value": -1}
         ]
     },
     {
         "mbti": "ei",
-        "question": "As you hunt you notice a familiar smell hanging in the air...",
+        "question": "You lay a trap for your prey. How do you set it up?",
         "answers": [
-            {"text": "The comforting stench of rotting bodies", "value": 1},
-            {"text": "The calming scent of incense and ash", "value": -1}
+            {"text": "Everything is in its place -- no room for error.", "value": 1},
+            {"text": "Put it where I'm sure they'll run.", "value": -1}
         ]
     },
     {
         "mbti": "ei",
-        "question": "Your victim attempts to fight back!",
+        "question": "Your prey pleads for their life.",
         "answers": [
-            {"text": "Amputate their arm... and relish the sounds of their screams", "value": 1},
-            {"text": "Let them try... they will learn the futility of their actions", "value": -1}
+            {"text": "It's just a job, nothing personal.", "value": 1},
+            {"text": "They say they’re a single father? Wait...", "value": -1}
         ]
     },
     {
         "mbti": "ei",
-        "question": "Your victim stumbles and falls. What do you do?",
+        "question": "Finally, there's nowhere to run.",
         "answers": [
-            {"text": "q10a1", "value": 1},
-            {"text": "q10a2", "value": -1}
-        ]
-    },
-    {
-        "mbti": "ei",
-        "question": "Your victim cries, begging you for mercy...",
-        "answers": [
-            {"text": "q11a1", "value": 1},
-            {"text": "q11a2", "value": -1}
-        ]
-    },
-    {
-        "mbti": "ei",
-        "question": "Finally, your victim hits a dead end",
-        "answers": [
-            {"text": "q12a1", "value": 1},
-            {"text": "q12a2", "value": -1}
+            {"text": "It was all planned--this was inevitable.", "value": 1},
+            {"text": "You've run enough--it's time to face your fate.", "value": -1}
         ]
     },
     {
